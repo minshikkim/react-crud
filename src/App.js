f7322b901e50d751e50f2c261cb8be0d3aedc3a1
unknown <minshik6905@gmail.com>
2020-07-01 17:18:04 +0900

    create new Component file - createContent.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,74 @@ import './App.css'; //APP 이라는 녀석의 디자인
 import TOC from "./components/TOC";
 import Content from "./components/content";
 import Subject from "./components/subject"; 
+import Controller from './components/control';
 
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      mode:'read',
+      selected_content_id:1,
+      subject:{title:'web', sub:'world wide web'},
+      welcome:{title:'welcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML IS HyperText ...'},
+        {id:2, title:'css', desc:'css is cascading style sheet ...'},
+        {id:3, title:'javascript', desc:'javascript is .. '}
+      ]
+    }
+  }
+
+  handleClick(){
+    this.setState({mode:'welcome'})
+  }
+  
+  
+
   render(){
+    console.log('App render');
+    var _title, _desc = null;
+
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode === 'read'){
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i+1;
+      }
+    }
+
+    console.log('render', this)
     return(
       <div className="App">
-        <Subject title="WEB" sub="world wide"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup language."></Content>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+          onChangePage={function() {
+            this.setState({mode: 'welcome'})
+          }.bind(this)}
+          >
+          </Subject>
+        <TOC 
+          data={this.state.contents}
+          onChangePage={function(id){
+            // debugger;
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            })
+          }.bind(this)}
+        ></TOC>
+        <Controller></Controller>
+        
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 
 class TOC extends Component{
-    render(){
-      return(
-        <nav>
-          <ul>
-              <li><a href="1.html">HTML</a></li>
-              <li><a href="2.html">CSS</a></li>
-              <li><a href="3.html">JavaScript</a></li>
-          </ul>
-        </nav>
-      );
-    }
-  }
+  render(){
+    console.log('TOC render');
+      var lists = [];
+      var data = this.props.data;
+      var i = 0;
+      while(i < data.length){
+          lists.push(
+            <li key={data[i].id}>
+              <a 
+                href={"/content/" + data[i].id}
+                data-id={data[i].id}
+                onClick={function(id, e){
+                  console.log(e.target);
+                  // debugger;
+                  e.preventDefault();
+                  this.props.onChangePage(id);
+                }.bind(this, data[i].id)}
 
-  export default TOC; // TOC 라 클래스를 가져다 쓸 수 있도록 export 한다 .
+              >{data[i].title}</a>
+            </li>);
+            i = i + 1;
+      }
+    return(
+      <nav>
+        <ul>
+            {lists}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default TOC; // TOC 라 클래스를 가져다 쓸 수 있도록 export 한다 .
